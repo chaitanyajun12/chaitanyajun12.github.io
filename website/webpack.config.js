bf3a481756cc6bf467ac45ee468b0c4a7b8d3fcc
Krishna Chaitanya M <chaitanya.jun12@gmail.com>
2020-04-10 09:30:36 +0530

    Added copy webpack plugin to copy images to dist folder.

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,18 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './js/index.js',
+    entry: path.resolve('js', 'index.js'),
     devtool: 'inline-source-map',
     devServer: {
         contentBase: '..',
         port: 8000
     },
     plugins: [
+        new CopyWebpackPlugin([{
+                from: path.resolve('images'), 
+                to: path.resolve('..', 'images')
+        }]),
         new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
+            template: 'index.html'
+        })
     ],
     output: {
         filename: 'bundle.js',
